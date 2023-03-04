e509eebbf6612632438ff8eacb972b2ff2719c6c
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-05 00:10:50 +0300

    fix: поправил проблему в регистрации

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 const Unauthorized = require('../errors/Unauthorized');
 
 const userSchema = new mongoose.Schema({
