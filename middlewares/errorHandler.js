1d31dfd0fb61c58e1f3647c46aa14e86e57e6d57
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-28 15:54:37 +0300

    fix: переписал errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,11 +2,13 @@ const INTERNAL_SERVER_ERROR = require('../errors/StatusCode');
 
 const errorHandler = (err, req, res, next) => {
   if (err.statusCode) {
-    res.status(err.statusCode).send({ message: err.message });
-  } else {
-    res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
+    return res.status(err.statusCode).send({ message: err.message });
   }
-  next();
+
+  return () => {
+    res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
+    next();
+  };
 };
 
 module.exports = errorHandler;
