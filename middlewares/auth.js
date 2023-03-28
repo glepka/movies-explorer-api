aa30d4df590933ba986142a1c181339f4af7e365
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-28 16:49:56 +0300

    fix: поправил аутентификацию

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,14 +7,13 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer')) {
-    throw new Unauthorized('Необходима авторизация!');
-  }
-
-  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
+    if (!authorization || !authorization.startsWith('Bearer')) {
+      throw new Unauthorized('Необходима авторизация!');
+    }
+    const token = authorization.replace('Bearer ', '');
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_DEV);
   } catch (err) {
     next(new Unauthorized('Необходима авторизация!'));
