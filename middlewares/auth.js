eb920d45cb02461668964494fd0aae6abcd172b1
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-29 14:06:34 +0300

    fix: заново исправил код auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,16 +5,15 @@ const { JWT_SECRET_DEV } = require('../utils/constants');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer')) {
-    throw new Unauthorized('Необходима авторизация!');
-  }
-
-  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer')) {
+      throw new Unauthorized('Необходима авторизация!');
+    }
+    const token = authorization.replace('Bearer ', '');
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_DEV);
   } catch (err) {
     next(new Unauthorized('Необходима авторизация!'));
