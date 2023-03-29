f442837c12f12b541ea8dbb92e09586a6cc3039f
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-29 16:28:07 +0300

    fix: поменял корс

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 const allowedCors = [
-  'http://api.movies.nomoredomains.work',
-  'https://api.movies.nomoredomains.work',
+  'http://glepka.movies.nomoredomains.work',
+  'https://glepka.movies.nomoredomains.work',
   'http://localhost:3000',
   'https://localhost:3000',
 ];
